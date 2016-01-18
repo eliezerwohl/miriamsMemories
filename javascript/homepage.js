e1e699c59fdb2bdc8e073020a2f07ab46c2378ca
eliezerwohl <eliezerwohl@gmail.com>
2016-01-18 16:12:31 -0500

    made site heroku ready

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -3,33 +3,33 @@ $("body").on("click", ".back", function(){
 });
 $("#loginButton").on("click", function(){
   $(".loginPanel").fadeOut();
-var col1 = $("<div>").addClass("col-md-6 btnDiv")
-var col2 = $("<div id='newPatient'>").addClass("col-md-6 btnDiv")
-var col3 = $("<div>").addClass("col-md-6 btnDiv")
-var col4 = $("<div>").addClass("col-md-6 btnDiv")
-var col5 = $("<div>").addClass("col-md-6 back").hide();
-
+  var colSearch = $("<div>").addClass("col-md-6 btnDiv")
+  var colNewPatient = $("<div id='newPatient'>").addClass("col-md-6 btnDiv")
+  var colViewAll = $("<div>").addClass("col-md-6 btnDiv")
+  var colLogOut = $("<div>").addClass("col-md-6 btnDiv")
+  var colBackButton = $("<div>").addClass("col-md-6 back").hide();
   var searchButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("search by patient").hide().fadeIn(2700);
   var newPatientButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("new patient").hide().fadeIn(3000);
   var viewAllButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("view all clients").hide().fadeIn(3300);
   var logOutButton = $("<button id='logout'>").addClass("btn btn-info btn-lg btn-block").append("logout").hide().fadeIn(3500);
   var backButton = $("<button>").addClass("btn btn-alert btn-lg btn-block").append("<span class='glyphicon glyphicon-arrow-left'></span>BACK");
 
-  $(col1).append(searchButton);
-  $(col2).append(newPatientButton);
-  $(col3).append(viewAllButton);
-  $(col4).append(logOutButton);
-  $(col5).append(backButton);
+  $(colSearch).append(searchButton);
+  $(colNewPatient).append(newPatientButton);
+  $(colViewAll).append(viewAllButton);
+  $(colLogOut).append(logOutButton);
+  $(colBackButton).append(backButton);
 
-  $('.target').append(col5)
-  .append(col1)
-  .append(col2)
-  .append(col3)
-  .append(col4);
+  $('.target').append(colBackButton)
+  .append(colSearch)
+  .append(colNewPatient)
+  .append(colViewAll)
+  .append(colLogOut);
 });
 $("body").on("click", "#logout", function(){
-location.reload();
+  location.reload();
 });
+
 $("body").on("click", ".btnDiv", function(e){
    e.preventDefault()
   $(".btnDiv").not(this).hide();
