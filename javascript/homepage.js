db5f95d660d5d208ec95747c04575be65bab5fd1
eliezerwohl <eliezerwohl@gmail.com>
2016-01-17 22:44:44 -0500

    made the hide target the div NOT the button, which makes it so when they are hidden they collapse correctly.

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,17 +1,36 @@
+
 $("#loginButton").on("click", function(){
   $(".loginPanel").fadeOut();
 
-
+var col1 = $("<div>").addClass("col-md-6 btnDiv")
+var col2 = $("<div>").addClass("col-md-6 btnDiv")
+var col3 = $("<div>").addClass("col-md-6 btnDiv")
+var col4 = $("<div>").addClass("col-md-6 btnDiv")
+var col5 = $("<div>").addClass("col-md-6 back").hide();
   // need a search by client
   // new client
   // view all clients
   // logout
-  var searchButton = $("<button>").addClass("btn btn-info btn-lg col-xs-12 col-md-6").append("search by patient").hide().fadeIn(3000);
-  var newPatientButton = $("<button>").addClass("btn btn-info btn-lg col-xs-12 col-md-6").append("new patient").hide().fadeIn(3500);
-  var viewAllButton = $("<button>").addClass("btn btn-info btn-lg col-xs-12 col-md-6").append("view all clients").hide().fadeIn(4000);
-  var logOutButton = $("<button>").addClass("btn btn-info btn-lg col-xs-12 col-md-6").append("logout").hide().fadeIn(4500);
-  $('.target').append(searchButton)
-  .append(newPatientButton)
-  .append(viewAllButton)
-  .append(logOutButton);
+  var searchButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("search by patient").hide().fadeIn(3000);
+  var newPatientButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("new patient").hide().fadeIn(3500);
+  var viewAllButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("view all clients").hide().fadeIn(4000);
+  var logOutButton = $("<button>").addClass("btn btn-info btn-lg btn-block").append("logout").hide().fadeIn(4500);
+  var backButton = $("<button>").addClass("btn btn-alert btn-lg btn-block").append("<span class='glyphicon glyphicon-arrow-left'></span>BACK");
+
+  $(col1).append(searchButton);
+  $(col2).append(newPatientButton);
+  $(col3).append(viewAllButton);
+  $(col4).append(logOutButton);
+  $(col5).append(backButton);
+
+  $('.target').append(col5)
+  .append(col1)
+  .append(col2)
+  .append(col3)
+  .append(col4);
+});
+
+$("body").on("click", ".btnDiv", function(){
+  $(".btnDiv").not(this).hide();
+  $(".back").fadeIn(3000)
 });
