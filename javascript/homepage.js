d89b968c96224e4349fa53050d444aefe6f98a37
eliezerwohl <eliezerwohl@gmail.com>
2016-01-23 20:29:51 -0500

    contact us button now works, needs content

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -3,7 +3,16 @@ $("#contact").on("click", function(e){
     e.preventDefault()
   $(".modalTitle, .modalBody, .modalFooter").empty();
   $(".modal").modal();
-  $(".modalHeader").append("Contact Us");
+  $(".modalTitle").append("Contact Us");
+  $(".modalBody").append("coming soon");
+  $(".modalFooter").append('<button type="submit" id="endModal" data-dismiss="modal" class="col-lg-12 col-xs-12 btn btn-lg btn-primary pull-left"><span class="glyphicon glyphicon-arrow-left"> </span>BACK</button>');
+});
+
+$("#about").on("click", function(e){
+    e.preventDefault()
+  $(".modalTitle, .modalBody, .modalFooter").empty();
+  $(".modal").modal();
+  $(".modalTitle").append("About Miriam's Memories");
   $(".modalBody").append("coming soon");
   $(".modalFooter").append('<button type="submit" id="endModal" data-dismiss="modal" class="col-lg-12 col-xs-12 btn btn-lg btn-primary pull-left"><span class="glyphicon glyphicon-arrow-left"> </span>BACK</button>');
 });
