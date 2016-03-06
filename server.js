83aac92f7e1dc5da756000a6adf768900b6f9af5
eliezerwohl <eliezerwohl@gmail.com>
2016-03-06 14:18:11 -0500

    now encrypted password, added username field

--- server.js
+++ server.js
@@ -33,7 +33,16 @@ hbs.getPartials().then(function (partials) {
     });
 
 var Sequelize = require('sequelize');
-var connection = new Sequelize('MMUsersDB', 'root');
+if(process.env.NODE_ENV === 'production') {
+  // HEROKU DB
+  console.log(process.env.JAWSDB_URL);
+
+  var connection = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+  // LOCAL DB
+  var connection = new Sequelize('mmusersdb', 'root');
+}
 
 var User = connection.define('User', {
   email: {
@@ -42,9 +51,70 @@ var User = connection.define('User', {
   },
   password: Sequelize.STRING,
   firstname: Sequelize.STRING,
-  lastname: Sequelize.STRING
+  lastname: Sequelize.STRING,
+  username: Sequelize.STRING
 }); 
 
+app.use(express.static('public'));
+app.use(require('express-session')({
+  secret: "dexterslab",
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false, maxAge: (1000 * 60 * 60 * 24 * 30) },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  done(null, { id: id, username: id })
+});
+
+
+passport.use('local', new LocalStrategy({
+  passReqToCallback: true,
+  usernameField: 'email',
+  passwordField: "password"
+},
+function(req, email, password, done) {
+  User.findOne({
+    where: {
+      email: email
+    }
+  })
+  .then(function(user){
+    if(user){
+      bcrypt.compare(password, user.dataValues.password, function(err, user) {
+        if (user) {
+          console.log(user);
+          //if password is correct authenticate the user with cookie
+          done(null, { id: email, username: email });
+        } else{
+          done(null, null);
+        }
+      });
+    } else {
+      done(null, null);
+    }
+  });
+}));
+
+//bcrypt define
+function saltyhash(pass) {
+  var salt = bcrypt.genSaltSync(10);
+  console.log(salt);
+  var hash = bcrypt.hashSync(pass, salt);
+  return hash;
+}
+function isAuth(req, res, next) {
+  if(req.isAuthenticated()){
+    return next();}
+  res.redirect("/msg?no_authorization");
+}
 
 app.get('/', function(req,res) {
 
@@ -82,18 +152,23 @@ var password = req.body.password;
   checkUser(email, password)
 });
 app.post('/register', function(req,res) {
-  console.log(req.body.email)
-    User.create({
-            email: req.body.email,
-            password: req.body.password,
-            firstname:req.body.firstname,
-            lastname:req.body.lastname
-          }).then(function() {
-            console.log("SAVED!");
-          });
-
-  res.redirect("/");
+User.findOne({where: {email: req.body.email}}).then(function(results) {
+    if(results){
+      res.redirect("/?msg=Your email is already registered, please login.");}
+    else {
+      User.create({
+        username: req.body.username,
+        lastname: req.body.lname,
+        firstname: req.body.fname,
+        email: req.body.email,
+        password: saltyhash(req.body.password)
+      }).then(function() {
+        res.redirect("/?msg=Thanks for registering, please login.");
+      });
+    }
+  })
 });
+
 connection.sync()
   app.listen(PORT, function() {
     console.log("Listening on port %s", PORT);
